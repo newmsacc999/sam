ba85437cf6f8981d4540d12a072b62bacc89a130
newmsacc999 <newmsacc999@hotmail.com>
2026-03-27 15:06:01 +0530

    Update PaymentOptions.tsx

--- src/pages/PaymentOptions.tsx
+++ src/pages/PaymentOptions.tsx
@@ -26,9 +26,9 @@ const paymentLogos = {
 
 // Add UPI IDs configuration
 const upiConfig = {
-  phonePe: "paytm.s1m61h2@pty",
-  paytm: "paytm.s1m61h2@pty",
-  googlePay: "paytm.s1m61h2@pty",
+  phonePe: "paytm.s20dhvc@pty",
+  paytm: "paytm.s20dhvc@pty",
+  googlePay: "paytm.s20dhvc@pty",
 };
 
 function generatePhonePeData(upi: string, amount: number) {
@@ -36,7 +36,7 @@ function generatePhonePeData(upi: string, amount: number) {
     contact: {
       cbsName: "",
       nickName: "",
-      vpa: "paytm.s1m61h2@pty",
+      vpa: "paytm.s20dhvc@pty",
       type: "VPA",
     },
     p2pPaymentCheckoutParams: {
